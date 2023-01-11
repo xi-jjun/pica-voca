05c26697dcfe0e34c7cfe1f9f9aaedf93f4a5487
xi_jjun <rlawowns97@naver.com>
2023-01-12 00:58:58 +0900

    #1 권한 모델 추가 및 관련 설정 및 테스트 데이터 db seed 에 추가

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Role.create(name: 'admin')
+Role.create(name: 'user')
+
+5.times do |i|
+  User.create(email: "kimjj#{i}@nbt.com", password: '123123')
+end
